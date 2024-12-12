73b71a1c2d2da6cf1ee358f3cc53426603cf4f19
Ajinkya Rathod <ajinzrathod@Ajinkyas-MacBook-Pro.local>
2024-12-12 18:06:36 +0530

    Centralized Swagger UI in api-gateway

--- api-gateway/src/main/java/com/ajinz/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ajinz/gateway/config/SecurityConfig.java
@@ -8,10 +8,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+  private final String[] freeResourceURLs = {
+    "/swagger-ui.html",
+    "/swagger-ui/**",
+    "/v3/api-docs/**",
+    "/api-docs/**",
+    "/aggregate/**",
+    "/swagger-resources/**",
+  };
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-        .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers(freeResourceURLs)
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
         .build();
   }
