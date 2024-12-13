06ddc68b2105539197c41ed2680c619cd3523fdd
ajinzrathod <rajinkya26@gmail.com>
2024-12-13 13:59:01 +0530

    Implement actuator and circuit breaker in order service

--- order-service/src/main/java/com/ajinz/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ajinz/microservices/order/client/InventoryClient.java
@@ -1,9 +1,22 @@
 package com.ajinz.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
+  Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
   @GetExchange("/api/inventory")
-  boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+  @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+  @Retry(name = "inventory")
+  Boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+  default Boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+    log.info("Cannot get inventory {}, failure reason: {}", code, throwable.getMessage());
+    return null;
+  }
 }
--- order-service/src/main/java/com/ajinz/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/ajinz/microservices/order/config/RestClientConfig.java
@@ -1,12 +1,26 @@
 package com.ajinz.microservices.order.config;
 
 import com.ajinz.microservices.order.client.InventoryClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -14,11 +28,40 @@ public class RestClientConfig {
   private String inventoryServiceURL;
 
   @Bean
-  public InventoryClient inventoryClient() {
-    RestClient restClient = RestClient.builder().baseUrl(inventoryServiceURL).build();
+  public InventoryClient inventoryClient()
+      throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    RestClient restClient =
+        RestClient.builder()
+            .baseUrl(inventoryServiceURL)
+            .requestFactory(getClientRequestFactory())
+            .build();
     RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
     HttpServiceProxyFactory httpServiceProxyFactory =
         HttpServiceProxyFactory.builderFor(restClientAdapter).build();
     return httpServiceProxyFactory.createClient(InventoryClient.class);
+  }
+
+  private ClientHttpRequestFactory getClientRequestFactory()
+      throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+    CloseableHttpClient httpClient =
+        HttpClients.custom()
+            .setConnectionManager(
+                PoolingHttpClientConnectionManagerBuilder.create()
+                    .setSSLSocketFactory(
+                        SSLConnectionSocketFactoryBuilder.create()
+                            .setSslContext(
+                                SSLContextBuilder.create()
+                                    .loadTrustMaterial(TrustAllStrategy.INSTANCE)
+                                    .build())
+                            .setHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+                            .build())
+                    .build())
+            .build();
+
+    HttpComponentsClientHttpRequestFactory requestFactory =
+        new HttpComponentsClientHttpRequestFactory(httpClient);
+    requestFactory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis());
+    requestFactory.setReadTimeout((int) Duration.ofSeconds(3).toMillis());
+    return requestFactory;
   }
 }
