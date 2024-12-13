9f3709cb29ff36eb6765e6b6209e052b3218135b
ajinzrathod <rajinkya26@gmail.com>
2024-12-13 14:02:14 +0530

    Add new health endpoint

--- order-service/src/main/java/com/ajinz/microservices/order/controller/OrderController.java
+++ order-service/src/main/java/com/ajinz/microservices/order/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.ajinz.microservices.order.controller;
 
 import com.ajinz.microservices.order.dto.OrderRequest;
 import com.ajinz.microservices.order.service.OrderService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,17 +9,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-    private final OrderService orderService;
+  private final OrderService orderService;
 
-    @Autowired
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
-    }
+  @Autowired
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order placed";
-    }
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public String placeOrder(@RequestBody OrderRequest orderRequest) {
+    orderService.placeOrder(orderRequest);
+    return "Order placed";
+  }
+
+  @GetMapping("/health")
+  @ResponseStatus(HttpStatus.OK)
+  public String health() {
+    return "Health OK";
+  }
 }
